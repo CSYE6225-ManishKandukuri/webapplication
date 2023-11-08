e1efe26a30aa43ffe77e871fe44e06c38e60c47e
Manish Kandukuri <kandukuri.m@northeastern.edu>
2023-11-08 11:47:48 -0500

    commit Updated

---
+++ test.js
@@ -0,0 +1,71 @@
+const request = require('supertest');
+
+const app = require('./app.js');
+
+const chai = require('chai');
+
+const expect = chai.expect;
+
+ 
+
+describe('Health Check Endpoint', function() {
+
+    it('Return 200 OK when the database is connected.', async function() {
+
+        const response = await request(app).get('/healthz');
+
+        expect(response.status).to.equal(200);
+
+     
+
+    });
+
+ 
+
+ 
+
+});
+
+
+ 
+
+ 
+
+
+// const request = require('supertest')
+// const app = require('./index.js');
+// const chai = require('chai')
+// const expect = chai.expect
+
+ 
+
+
+//     describe('Successes', function() {
+//         it('Return the product if the product name is valid', function(done) {
+//             request(app).get('/healthz').send({ }).end(function(err, res) {
+//                 expect(res.statusCode).to.be.equal(200)
+//                 done()
+//             })
+//         })
+//     })
+
+// const request = require('supertest');
+// const app = require('./index.js');
+// const chai = require('chai');
+// const expect = chai.expect;
+
+// describe('Successes', function() {
+//     it('Return the product if the product name is valid', function(done) {
+//         request(app)
+//             .get('/healthz')
+//             .send({})
+//             .end(function(err, res) {
+//                 if (err) {
+//                     done(err); // Handle and pass the error to done
+//                 } else {
+//                     expect(res.statusCode).to.be.equal(200);
+//                     done(); // Indicate the test is complete
+//                 }
+//             });
+//     });
+// });
